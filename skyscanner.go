16b87c0c71fdc14abe5d3f4286665176f8717752
ricky_lin <ricky_lin@trendmicro.com>
2025-02-23 16:15:12 -0600

    go...

--- skyscanner.go
+++ skyscanner.go
@@ -8,17 +8,15 @@ type Skyscanner struct {
 }
 
 func NewSkyscannerProvider(apiKey string) *Skyscanner {
-	// 使用 apiKey 初始化 Skyscanner
 	return &Skyscanner{apiKey: apiKey}
 }
 
 func (p *Skyscanner) GetFlightFees(from, to string) (map[string]interface{}, error) {
-	// 实现获取费用的逻辑
 	if from == "" || to == "" {
 		return nil, errors.New("invalid input")
 	}
 	return map[string]interface{}{
 		"provider": "Skyscanner",
-		"fee":      200.0, // 示例返回值
+		"fee":      200.0,
 	}, nil
 }
