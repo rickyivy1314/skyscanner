fda492ff5d72e838a9e2f251cfc6a5f29f53532a
ricky_lin <ricky_lin@trendmicro.com>
2025-02-24 02:09:06 -0600

    go...

--- skyscanner.go
+++ skyscanner.go
@@ -1,22 +1,49 @@
 package skyscanner
 
-import "errors"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 // Skyscanner implements the FlightFeeProvider interface
-type Skyscanner struct {
+type SkyscannerProvider struct {
 	apiKey string
 }
 
-func NewSkyscannerProvider(apiKey string) *Skyscanner {
-	return &Skyscanner{apiKey: apiKey}
+func NewSkyscannerProvider(apiKey string) *SkyscannerProvider {
+	return &SkyscannerProvider{apiKey: apiKey}
 }
 
-func (p *Skyscanner) GetFlightFees(from, to string) (map[string]interface{}, error) {
-	if from == "" || to == "" {
-		return nil, errors.New("invalid input")
+// GetFlightFees fetches flight fees from the Skyscanner API
+func (p *SkyscannerProvider) GetFlightFees(from, to string) (map[string]interface{}, error) {
+	apiURL := fmt.Sprintf("https://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/US/USD/en-US/%s/%s/2023-12-25?apiKey=%s", from, to, p.apiKey)
+
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %v", err)
 	}
-	return map[string]interface{}{
-		"provider": "Skyscanner",
-		"fee":      200.0,
-	}, nil
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching data: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: received status code %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, fmt.Errorf("error unmarshalling response: %v", err)
+	}
+
+	return result, nil
 }
